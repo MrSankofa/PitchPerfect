0394b4e6f3ce5fceb7ebc7a346fbf0c28ca7c246
Brett Cunningham <bwcunninghamii@gmail.com>
2018-11-18 15:39:15 -0500

    add navigation controller and view controller

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         stopRecordingButton.isEnabled = false
-        recordButton.isEnabled = true
     }
     
     override func didReceiveMemoryWarning() {
