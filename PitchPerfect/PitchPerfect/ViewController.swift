0ab1a86e9aaf9b85f1db4cf4fdfe75caf35c40c8
Brett Cunningham <bwcunninghamii@gmail.com>
2018-11-18 15:14:25 -0500

    add xcode proj and hello world

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Brett Cunningham on 11/18/18.
+//  Copyright © 2018 Altruistic Software Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
