c472484f331de0bff8b87c8dfbd254120eafc4f8
Brett Cunningham <bwcunninghamii@gmail.com>
2018-11-18 15:25:34 -0500

    Added IBOutlets IBActions for buttons and labels

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record Button was Pressed")
+        recordLabel.text = "Recording In Progress"
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording Button Was Pressed")
+        recordLabel.text = "Tap To Record"
+    }
+    
 }
 
